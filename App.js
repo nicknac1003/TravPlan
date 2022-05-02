6fec63d9c0b285db09da4b61aa87008b337ee6bc
Kimaya <47405612+kimayamcolaco@users.noreply.github.com>
2022-05-02 03:20:22 -0400

    Update App.js

--- App.js
+++ App.js
@@ -5,10 +5,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
+import SwipeScreen from './screens/SwipeScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const swipesRef = useRef(null)
 
   return (
     <NavigationContainer>
@@ -16,9 +19,42 @@ export default function App() {
         <Stack.Screen options={{headerShown: false}} name='Login' component={LoginScreen}/>
         <Stack.Screen options={{headerShown: false}} name='Register' component={RegisterScreen}/>
         <Stack.Screen options={{headerShown: false}} name='Home' component={HomeScreen}/>
+        <Stack.Screen options={{headerShown: false}} name='SwipeScreen' component={SwipeScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
+
+//   function handleLike() {
+//     console.log('like')
+//     nextUser()
+//   }
+
+//   function handlePass() {
+//     console.log('pass')
+//     nextUser()
+//   }
+
+//   function nextUser() {
+//     const nextIndex = users.length - 2 === currentIndex ? 0 : currentIndex + 1
+//     setCurrentIndex(nextIndex)
+//   }
+
+
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.swipes}>
+//           <SwipeScreen
+//             ref={swipesRef}
+//             currentIndex={currentIndex}
+//             users={users}
+//             handleLike={handleLike}
+//             handlePass={handlePass}
+//           ></SwipeScreen>
+              
+//       </View>
+//     </View>
+//   )
+
 }
 
 const styles = StyleSheet.create({
